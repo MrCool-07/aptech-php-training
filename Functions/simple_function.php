7367d14cdf87a1f0c713adeecbd7e06000f128df
Pradeep Ketkale <pbketkale@gmail.com>
2020-02-02 12:13:06 +0530

    Added function example, folder for session management

---
+++ Functions/simple_function.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+1. Function Declaration - function add($var1, $var2)
+	- Function Name
+	- Function parameters/arguments
+2. Function Definition
+	function add($var1, $var2){
+		$var3 = $var1 + $var2;
+		echo $var3;	
+	} 
+3. Function Body
+	- Set of Instructions
+	$var3 = $var1 + $var2;
+	echo $var3;
+4. Function call
+	- function name 
+	- arguments
+	e.g. add(3,4);
+*/
+
+$var1 = 4;
+$var2 = 5;
+
+add($var1, $var2);
+
+function add($var1, $var2){
+	$var3 = $var1 + $var2;
+	echo $var3;
+}
+
+?>
