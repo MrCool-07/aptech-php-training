f878ab382249f93a41ae90887591c6c8f6ce5ae4
ADI-KOTKAR <adityakotkar75@gmail.com>
2020-02-15 16:25:28 +0530

    002 done

--- ADI-KOTKAR_PHP/Assignment/002.php
+++ ADI-KOTKAR_PHP/Assignment/002.php
@@ -3,15 +3,10 @@ Sample string : '082307'
 Expected Output : 08:23:07 -->
 <?php
     $str = '082307';
-    $a = ':';
-    $b = 2;
-    echo 'Input: <br>'.$str.'<br> Output: <br>';
-    substr_replace($str,':',$b,0);
-    echo $str;
-    // for($i=0;$i<strlen($str);$i++){
-        
-    //     if($i>2 && $i%2==0)
-
-    // }
+    echo $str.'<br>';
+    $split = str_split($str,2);
+    
+    echo implode(":",$split);
+    
 
 ?>
