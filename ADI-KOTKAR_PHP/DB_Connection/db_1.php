698ef97f7f8d29082d100719ac03bfde2ec0611c
ADI-KOTKAR <adityakotkar75@gmail.com>
2020-02-23 17:13:03 +0530

    to close connection, delete $connect from memory

--- ADI-KOTKAR_PHP/DB_Connection/db_1.php
+++ ADI-KOTKAR_PHP/DB_Connection/db_1.php
@@ -7,9 +7,10 @@
     $connect = mysqli_connect($host,$username,$password,$db);
 
     if(!$connect){
-        die('Could not make connection');
+        die('Could not make connection'.mysqli_error());
     }
     else{
         echo "Successfully made";
+        mysqli_close($connect);
     }
 ?>
