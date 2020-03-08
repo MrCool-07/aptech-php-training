3a999ea3bb1a4c9798155a0b49039b871cb2d9cc
Pradeep Ketkale <pbketkale@gmail.com>
2020-03-08 19:27:17 +0530

    Send email references

--- email/send_email.php
+++ email/send_email.php
@@ -1,7 +1,21 @@
 <?php
+
+/*
+
+Reference:
+XAMPP Settings:
+https://meetanshi.com/blog/send-mail-from-localhost-xampp-using-gmail/
+
+Google Settings:
+1. Go to Manage My Account
+2. Go to Security Tab
+3. Enable 2-Step Authentication
+4. Generate app password - used in sendmail.ini
+*/
+
 $to_email = "pbketkale@gmail.com";
 $subject = "Simple Email Test via PHP";
-$body = "Hi,nn This is test email send by PHP Script";
+$body = "Hi,This is test email send by PHP Script";
 $headers = "From: sender\'s email";
  
 if (mail($to_email, $subject, $body, $headers)) {
